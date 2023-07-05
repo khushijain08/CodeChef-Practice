89f0e7ea4e998f1afcbb53c95ae538e0436fc5ba
khushijain08 <138589205+khushijain08@users.noreply.github.com>
2023-07-05 15:51:04 +0530

    Create Chef_Plays_Ludo_LUDO.cpp

---
+++ Chef_Plays_Ludo_LUDO.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,n,c;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+	    cin>>c;
+	    if(c==6)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
