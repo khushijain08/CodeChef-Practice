468e7e074fb4410afd652dcb9aa6fee22c57dd70
khushijain08 <138589205+khushijain08@users.noreply.github.com>
2023-07-09 17:40:02 +0530

    Create Just_One_More_Episode_ONEMORE.cpp

---
+++ Just_One_More_Episode_ONEMORE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,x;
+	cin>>n;
+	while(n--)
+	{
+	    cin>>x;
+	    if(x>24)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
