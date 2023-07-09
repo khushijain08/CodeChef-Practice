844a36dc1ffbd8a8ce25367b7ae9d1bc8e8836c6
khushijain08 <138589205+khushijain08@users.noreply.github.com>
2023-07-09 16:04:41 +0530

    Create Total_Prize_Money_PRIZEPOOL.cpp

---
+++ Total_Prize_Money_PRIZEPOOL.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i,x,y;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+	    cin>>x;
+	    cin>>y;
+	    cout<<10*x+90*y<<endl;
+	}
+	return 0;
+}
