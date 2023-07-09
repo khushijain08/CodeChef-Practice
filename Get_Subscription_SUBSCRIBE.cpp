8941bbe38bf95f3bdd2a3086b128be488310e342
khushijain08 <138589205+khushijain08@users.noreply.github.com>
2023-07-09 17:10:14 +0530

    Create Get_Subscription_SUBSCRIBE.cpp

---
+++ Get_Subscription_SUBSCRIBE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i,x;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+	    cin>>x;
+	    if(x>30)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
