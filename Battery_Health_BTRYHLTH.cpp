7107f25b94dcfc5f930e7bd595ff77355d0f176d
khushijain08 <138589205+khushijain08@users.noreply.github.com>
2023-07-09 16:11:12 +0530

    Create Battery_Health_BTRYHLTH.cpp

---
+++ Battery_Health_BTRYHLTH.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,i,x;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+	    cin>>x;
+	    if(x>=80)
+	    cout<<"YES"<<endl;
+	    else
+	    cout<<"NO"<<endl;
+	    
+	}
+	return 0;
+}
