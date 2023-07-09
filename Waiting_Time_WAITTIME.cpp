94a84171f2d440ab2d168d0e57fd43607a5fa658
khushijain08 <138589205+khushijain08@users.noreply.github.com>
2023-07-09 17:33:52 +0530

    Create Waiting_Time_WAITTIME.cpp

---
+++ Waiting_Time_WAITTIME.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long int tc , k , x ;
+	cin>>tc;
+	while(tc--){
+	      cin>>k>>x;
+	      cout<<7*k-x<<endl;
+	}
+	return 0;
+}
