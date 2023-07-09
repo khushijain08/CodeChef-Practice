0070cc7854a5da59a79d4d5d46b1d74e2464dcfa
khushijain08 <138589205+khushijain08@users.noreply.github.com>
2023-07-09 17:01:57 +0530

    Create Number_of_Credits_CREDS.cpp

---
+++ Number_of_Credits_CREDS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i,n,x,y,z;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+	    cin>>x;
+	    cin>>y;
+	    cin>>z;
+	    cout<<x*4+y*2+z*0<<endl;
+	}
+	return 0;
+}
